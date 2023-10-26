65adc7b4f7430f5d0b86dc3d2a1e83cf9a7b024a
Jake <jakerh@hey.com>
2023-10-26 13:12:52 -0500

    next

--- home.nix
+++ home.nix
@@ -34,35 +34,7 @@
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
   # incompatible changes.
-	programs.zsh = {
-		enabled = true;
-		initExtra = "
-fish
-";
-	};
-	programs.fish = {
-		enable = true;
-		initExtra = @"
-if status is-interactive
-  set fish_greeting
-  alias calc=\"emacsclient -c -n -q -e \(full-calc\)\"
-  alias icat=\"kitty +kitten icat\"
-	alias renix=\"darwin-rebuild switch --flake ~/.config/nix-darwin/\"
-	alias upnix=\"cd .config/nix-darwin/;nix flake update;cd ~/\"
-end
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/jakehackl/miniconda3/bin/conda
-    eval /Users/jakehackl/miniconda3/bin/conda \"shell.fish\" \"hook\" $argv | source
-end
-# <<< conda initialize <<<
-bass source \"$HOME/.cargo/env\"
-set BUN_INSTALL \"$HOME/.bun\"
-set PATH \"$BUN_INSTALL/bin:/opt/pkg/bin:$PATH\"
-set MANPATH \"/opt/pkg/man:$MANPATH\"
-"
-		}
   home.sessionVariables.EDITOR = "nvim";
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
